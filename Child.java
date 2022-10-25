a64784b929c27450189b43aaa128a04a00c6f23c
rurimutiara <114370768+rurimutiara@users.noreply.github.com>
2022-10-25 16:49:14 +0700

    Add files via upload

--- Child.java
+++ Child.java
@@ -1,7 +1,10 @@
-class Child extends Parent {
-    public void Info(int x) {
-        System.out.println("Nilai x sebagai parameter = " + x);
-        System.out.println("Data member x di class Child = " + this.x);
-        System.out.println("Data member x di class Parent = " + super.x);
- }
+public class Parent {
+
+public class Child extends Parent {
+    int x;
+    public Child() {
+        x = 5;
+    }
+}
+
 }
